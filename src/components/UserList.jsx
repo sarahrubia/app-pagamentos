866ad4bce9f77f5a28f473a6e387637411d88987
sarahrubia <sarahrubia22@gmail.com>
2022-05-05 06:59:06 -0300

    imprimindo componente na tela

---
+++ src/components/UserList.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// Acessar a pasta react-context que tem um exemplo de contexto, paginas, componentes 
+
+export default class UserInfo extends Component {
+
+    render () {
+        return (
+            <>
+                <img src="https://via.placeholder.com/100" alt="" />
+                <p>Nome do usuário:</p>
+                <p>ID:</p>
+                <p>Username:</p>
+                <button>Pagar</button>
+            </>
+        )
+    }
+}
---
+++ src/interfaces/IUserInfo.tsx
@@ -0,0 +1,6 @@
+export default class User {
+    name:string;
+    id: number;
+    username:string;
+
+}
