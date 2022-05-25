07e05817f30451e4392331bd29fc56095dd99ccc
sarahrubia <sarahrubia22@gmail.com>
2022-05-25 18:10:11 -0300

    feat: popular select a partir de um array de obj

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import styled, { css } from "styled-components";
-// import PostForm from "../PostForm";
+// import axios from 'axios';
 
 const Button = styled.button`
   cursor: pointer;
@@ -63,38 +63,64 @@ Modal.setAppElement("#root");
 
 export default function PaymentModal(props) {
   const [isOpen, setIsOpen] = useState(false);
+  // const [paymentInfo, setPaymentInfo] = useState({
+  //   destination_user_id: "",
+  //   payment_value: "",
+  //   card_number: "",
+  //   cvv: "",
+  //   expiry_date: ""
+  // });
 
-  function toggleModal() {
+  const mask = (e) => {
+    e.preventDefault();
+
+    if (/[0-9]+/g.test(e.key) && e.target.value.length < 14) {
+      e.target.value += e.key;
+    }
+
+    let inputValue = Number(e.target.value.replace(/[^0-9]+/g, ""));
+    inputValue = inputValue / 100;
+    let inputFormat = inputValue.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    });
+
+    e.target.value = inputFormat;
+  } 
+
+  let cards = [
+    // valid card
+    {
+        card_number: "1111111111111111",
+        cvv: 789,
+        expiry_date: "01/18",
+    },
+    // invalid card
+    {
+        card_number: "4111111111111234",
+        cvv: 123,
+        expiry_date: "01/20",
+    },
+];
+
+
+  const toggleModal = () => {
     setIsOpen(!isOpen);
   };
 
-  const selectOptions = () => {
-    
-    let cards = [
-      // valid card
-      {
-        card_number: '1111111111111111',
-        cvv: 789,
-        expiry_date: '01/18',
-      },
-      // invalid card
-      {
-        card_number: '4111111111111234',
-        cvv: 123,
-        expiry_date: '01/20',
-      },
-    ];
-    
-    for (let i = 0; i < cards.length; i++) {
-      let selectCard = document.getElementById("selected");
-      let option = document.createElement("option");
-      let cardNumber = cards[i].card_number;
-      option.setAttribute("card_number", cardNumber);
-      option.textContent = "Cartão com final " + cardNumber.slice(cardNumber.length - 4);
-      selectCard.appendChild(option); 
-    }
-  }
-  
+  // submitHandler = e => {
+  //     e.preventDefault()
+  //     console.log(paymentInfo)
+
+  //     axios.post('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989', paymentInfo)
+  //       .then(response => {
+  //           console.log(response)
+  //       })
+  //       .catch(error => {
+  //           console.log(error)
+  //       })
+  // }
 
   return (
     <div className="PaymentModal">
@@ -115,10 +141,14 @@ export default function PaymentModal(props) {
             Pagamento para <PaymentHeaderUser>{props.name}</PaymentHeaderUser>
           </PaymentHeader>
           <PaymentForm>
-            <PaymentInput type="text" placeholder="R$ 0,00"></PaymentInput>
-            <PaymentSelect 
-              id="selected">
-                {selectOptions()}
+            <PaymentInput 
+              type="text" 
+              placeholder="R$ 0,00"
+              onKeyDown={mask} />
+            <PaymentSelect>
+            {cards.map((card) => (
+                <option key={card.card_number}>Cartão com final {card.card_number.slice(-4)}</option>
+            ))}
             </PaymentSelect>
           </PaymentForm>
           <ButtonDiv>
